93724b3710c3f57037c9f28f89c4d09c8ba94c33
MaxYMus <zenchenko91@mail.ru>
2018-07-04 01:01:18 +0700

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_example
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string SerName { get; set; }
+        public string Familia { get; set; }
+    }
+}
